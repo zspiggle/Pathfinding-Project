05104ce77c7ca86ede049df12d8477380505be10
Zachary Spiggle <zspiggle@outlook.com>
2022-01-26 13:00:01 -0500

    Prototype of field area
    v1.0, lacks some functionality

--- control.py
+++ control.py
@@ -2,18 +2,86 @@ import tkinter as tk
 from app import Window
 from field import Tile
 
-def draw_tile(w):
-  w.canvas.create_rectangle(30, 10, 120, 80, outline="#fb0", fill="#fb0")
+from array import *
 
+tile_size = 35
+grid_size = 20
+spacing = 0
+start_offset = 20
+
+tiles = []
+#Init array
+#refactor as 2d array
+
+"""
+Colors:
+
+light Green - Goal    Hex:#399E5A
+
+Light Blue - Empty Tile Hex: #A8E0FF
+Dark blue = Wall tile   Hex: #345995
+Yellow - Inspected tile  Hex: #F0E100
+Purple - Selected Tile    Hex: 82204A
+Green - start   Hex: 26532B
+
+
+"""
+
+#def draw_tile(winndow, x=0, y=0):
+  #winndow.canvas.create_rectangle(x, y, x+tile_size, y+tile_size, outline="#000", fill="#0AB2E5")
+
+def draw_tile(winndow, tile):
+  xPos = tile.positionX * tile_size
+  yPos = tile.positionY * tile_size
+  winndow.canvas.create_rectangle(xPos, yPos, xPos+tile_size, yPos+tile_size, outline="#000", fill=tile.get_color())
+
+def find_tile(posX, posY):
+  for tile in tiles:
+    if ((tile.positionX == posX) and (tile.positionY == posY)):
+      return tile
+
+def redraw(win):
+  for tile in tiles:
+    draw_tile(win, tile)
+
+#Sets up tkinter
 def main():
   root = tk.Tk()
   win = Window()
 
-  root.geometry("400x100+300+300")
+
+  for x in range(0, grid_size-1):
+    for y in range(0, grid_size-1):
+      newTile = Tile(x,y,1)
+      #draw_tile(win, newTile)
+      tiles.append(newTile)
+
+
+
+  #print(tiles)
+
+  for i in range(0, grid_size -1):
+      t = find_tile(i,0)
+      if t != None:
+        t.set_type(1)
+      print(t)
+      t = find_tile(i,grid_size-1)
+      if t != None:
+        t.set_type(1)
+      print(t)
+
+  
+
+  #root.geometry("400x100+300+300")
+
+  redraw(win)
+
+  win.canvas.pack(fill=tk.BOTH, expand=1) #Doesnt do anything
+  win.pack(fill=tk.BOTH, expand=1)
+
   root.mainloop()
 
-  root.after(100, draw_tile, win) #needs to be threaded
-                                  #https://stackoverflow.com/questions/459083/how-do-you-run-your-own-code-alongside-tkinters-event-loop has threading example
+
 
 
 if __name__ == '__main__':
--- field.py
+++ field.py
@@ -1,11 +1,46 @@
-from turtle import Vec2D, position
+
+
+
 
 
 class Tile:
 
-  position = None
+  positionX = 0
+  positionY = 0
+  tileType = 0
 
-  def __init__(self, pos):
-    position = pos
+  selected = False
+  inspected = False
+
+  """
+  Types:
+  0-empty
+  1-wall
+  2-goal
+  3-start
+  """
+
+  def get_color(self):
+    if self.selected:
+      return "#82204A"
+    elif self.inspected:
+      return "#F0E100"
+    else:
+      match self.tileType:
+        case 0:
+          return "#A8E0FF"
+        case 1:
+          return "#345995"
+        case 2:
+          return "#399E5A"
+        case 3:
+          return "#26532B"
+    
+  def set_type(self, t=0):
+    self.tileType = t
+
+  def __init__(self, posX=0, posY=0, type=0):
+    self.positionX = posX
+    self.positionY = posY
 
   

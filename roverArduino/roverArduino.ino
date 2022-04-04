10d517d54d71ea44e2549633f50ed670cd52b011
Zachary Spiggle <zspiggle@outlook.com>
2022-04-03 22:18:37 -0400

    added arduino program

---
+++ roverArduino/roverArduino.ino
@@ -0,0 +1,49 @@
+#include <SabertoothSimplified.h>
+
+SabertoothSimplified Steve; 
+int MAX_POWER = 25; //This equates to ~ 5v, present max for testing
+
+int throttle(int power){
+
+  if(power > MAX_POWER)
+  {
+    return 25;
+  }
+  
+  else if(power < -MAX_POWER)
+  {
+    return -25; 
+  }
+
+  return power; 
+}
+
+
+void setup() {
+    Serial3.begin(9600);
+
+    SabertoothTXPinSerial.begin(9600); 
+  
+    Steve.drive(0);
+    Steve.turn(0);
+
+}
+
+void loop() {
+  if (Serial3.available()){
+    String command = Serial3.readString();
+
+    //f for forward
+    if (command == "f"){
+      Steve.drive(throttle(25));
+      Steve.turn(0);
+    //r for turn right
+    } else if (command == "r"){
+      Steve.turn(64);
+    //l for turn left
+    } else if (command == "l"){
+      Steve.turn(-64);
+    }
+  }
+
+}
